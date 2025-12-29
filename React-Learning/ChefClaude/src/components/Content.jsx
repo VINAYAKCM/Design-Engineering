74d4aabc18890b24abaf64850955cb8cee66ca05
cmvinayak <cmvinayak04@gmail.com>
2025-12-29 17:45:54 +0530

    Used array updation concept to get ingredient item input from user, extracted the input and used states to update the array and display back the ingredient to the user.

--- React-Learning/ChefClaude/src/components/Content.jsx
+++ React-Learning/ChefClaude/src/components/Content.jsx
@@ -1,15 +1,43 @@
+import React from "react"
+
 export default function Content() {
+
+    const [ingredients, setIngredients] = React.useState([])
+
+    const showIngredients = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+
+        const formData = new FormData(event.target)
+        const ingredient = formData.get("ingredient")
+
+        setIngredients(prevIngredients => [
+            ...prevIngredients, ingredient])
+    }
+
+    
     return (
         <main>
-            <form className ="ingredient-form">
+            <form onSubmit={handleSubmit} className ="ingredient-form">
                 <input 
                     aria-label="Add ingredient" 
                     type="text" 
                     className="input-box" 
-                    placeholder="eg. oregano" />
+                    placeholder="eg. oregano" 
+                    name="ingredient"
+                    />
                 
                 <button className="add-button">Add Ingredient</button>
             </form>
+
+            <ul className="ingredient-list">
+                {showIngredients}
+            </ul>
+
         </main>
     )
 }
+
