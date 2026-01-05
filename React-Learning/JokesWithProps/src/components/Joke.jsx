3ec9122c92a25c4cfd099bdc2992427829533b6d
cmvinayak <cmvinayak04@gmail.com>
2026-01-05 21:53:56 +0530

    Implemented Conditonal rendering to show punchline for jokes upon OnClick button press.

--- React-Learning/JokesWithProps/src/components/Joke.jsx
+++ React-Learning/JokesWithProps/src/components/Joke.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+
 export default function Joke(props) {
+
+  const [isShown, setIsShown] = React.useState(false);
+
+  function Toggle(){
+    setIsShown((prevState) => !prevState);
+  }
+
   return (
     <div>
-      <ul className="jokes">
-        <li className="setup">Setup: {props.setup}</li>
-        <p className="punch">Punchline: {props.punchline}</p>
-      </ul>
+        {props.setup &&  <h3>{props.setup}</h3>}
+        {isShown && <p>{props.punchline}</p>}
+        <button onClick={Toggle}>Show punchline</button>
     </div>
   );
 }
