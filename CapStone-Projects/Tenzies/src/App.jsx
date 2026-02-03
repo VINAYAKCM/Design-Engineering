671a0a142ff8c8ad28dfb6d05d7c69a52951efd2
cmvinayak <cmvinayak04@gmail.com>
2026-02-03 20:54:37 +0530

    Added 'click to freeze' die component and change value of other unfreezed die button by clicking roll button to generate random numbers for just those die buttons.

--- CapStone-Projects/Tenzies/src/App.jsx
+++ CapStone-Projects/Tenzies/src/App.jsx
@@ -1,33 +1,59 @@
 import './App.css'
 import Die from "../src/components/Die.jsx"
 import React from "react"
+import {nanoid} from "nanoid"
 
 export default function App() {
 
+  //States
   const [dieValue, setDieValue] = React.useState(generateAllNewDice())
-  const diceElements = dieValue.map(num => <Die value={num}/>)
   
+  //Generating random dice values
   function generateAllNewDice(){
     const newDice = []
     for (let i = 1; i <= 10; i = i+1){
       const randomNumber = Math.floor(Math.random() * 6) + 1;
-      newDice.push(randomNumber)
+      newDice.push({value: randomNumber, 
+                    isHeld: false,
+                    id: nanoid()})
     }
     return newDice;
     }
+
+    //Displaying Dice value on the button with properties
+    const diceElements = dieValue.map(dieObj => 
+      <Die 
+        key={dieObj.id}
+        value={dieObj.value} 
+        isHeld={dieObj.isHeld}
+        hold={hold}
+        id={dieObj.id}
+      />)
   
-  
+   //Generate new random values for each die button based on its isHeld value
     function rollDice(){
-      setDieValue(generateAllNewDice()) //So setDieValue is literally a function you call when
-                                        //you want to change the value of dieValue and generate
-                                        //returns the new random array since the generateAllNewDice
-                                        //returns as the output
+      setDieValue(oldDice =>
+        oldDice.map(die => {
+          return die.isHeld  
+            ? die
+            : {...die, value: Math.floor(Math.random() * 6) + 1}
+      })) 
     }
 
+    //Toggle diceValue on/off based on matching id
+    function hold(id){
+      setDieValue(oldDice => 
+        oldDice.map(die => {
+          return die.id === id ? {...die, isHeld: !die.isHeld} : die
+      })) 
+    }
 
   return (
     <>
       <main>
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
         <div className="die-container">
             {diceElements}
         </div>
--- CapStone-Projects/Tenzies/src/components/Die.jsx
+++ CapStone-Projects/Tenzies/src/components/Die.jsx
@@ -1,6 +1,12 @@
 export default function Die(props){
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "#FFFFFF"
+    }
+
     return(
-    <button className="btn">
+    <button className="btn" 
+            style = {styles}
+            onClick={() => props.hold(props.id)}>
         {props.value}
     </button>
     )
