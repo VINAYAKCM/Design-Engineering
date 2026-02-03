5f842b5e731e51e75ed9bf2ef94865a78236b075
cmvinayak <cmvinayak04@gmail.com>
2026-02-03 13:06:02 +0530

    Created a function for generating 10 random numbers between 1-6 and mapped over them to display the numbers on each button.

--- CapStone-Projects/Tenzies/src/App.jsx
+++ CapStone-Projects/Tenzies/src/App.jsx
@@ -1,26 +1,33 @@
 import './App.css'
 import Die from "../src/components/Die.jsx"
+import React from "react"
 
 export default function App() {
+
+  const [dieValue, setDieValue] = React.useState(generateAllNewDice())
+  
+  function generateAllNewDice(){
+    const newDice = []
+    for (let i = 1; i <= 10; i = i+1){
+      const randomNumber = Math.floor(Math.random() * 6) + 1;
+      newDice.push(randomNumber)
+    }
+    return newDice;
+    }
+  
+  const diceElements = dieValue.map(num => <Die value={num}/>)
 
   return (
     <>
       <main>
         <div className="die-container">
-          <Die value = {1} />
-          <Die value = {2} />
-          <Die value = {3} />
-          <Die value = {4} />
-          <Die value = {5} />
-          <Die value = {6} />
-          <Die value = {7} />
-          <Die value = {8} />
-          <Die value = {9} />
-          <Die value = {10} />
+            {diceElements}
         </div>
       </main>
     </>
   )
 }
+
+
 
  
