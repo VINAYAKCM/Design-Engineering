a91dd25cc2b9214a0cddeffa59945f69f92c2386
cmvinayak <cmvinayak04@gmail.com>
2026-02-22 20:16:36 +0530

    Practice successful. Created Tenzies from scratch. Went better than hoped.

--- Practice/tenzies/src/App.jsx
+++ Practice/tenzies/src/App.jsx
@@ -2,10 +2,15 @@ import './App.css'
 import Die from "/Users/cmvinayak/Documents/Design Engineering/Practice/tenzies/src/components/die.jsx"
 import {nanoid} from "nanoid";
 import { useState } from 'react';
+import Confetti from "react-confetti"
 
 export default function App() {
 
   const [dice, setDice] = useState(() => generateRandomDice())
+
+  const gameWon = dice.every(die => die.isHeld) && 
+        dice.every(die => die.value === dice[0].value)
+    
 
   function generateRandomDice(){
     const dieValues = []
@@ -24,15 +29,39 @@ export default function App() {
          value={die.value}
          isHeld={die.isHeld}
          id={die.id} 
+         holdDice={holdDice}
     />
   )
+
+  function rollDice(){
+    if (!gameWon) {
+          setDice(prevDice => prevDice.map(die => {
+          return die.isHeld ? die : {...die, value: Math.floor(Math.random() * 6) + 1}
+    }))
+        }
+        else {
+            setDice(generateRandomDice())
+        }
+  }
+
+  function holdDice(id){
+    setDice(prevDice => prevDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : die
+    }))
+  }
 
   return (
     <>
       <main>
+        {gameWon && <Confetti />}
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
         <div className='die-container'>
             {diceElements}
         </div>
+        <button className='roll-btn' onClick={rollDice}>
+          {gameWon ? "New Game" : "Roll"}
+        </button>
       </main>
     </>
   )
--- Practice/tenzies/src/components/die.jsx
+++ Practice/tenzies/src/components/die.jsx
@@ -1,6 +1,10 @@
 export default function Die(props){
-    return(
-        <button className="die-button">
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
+
+    return(                                                    // This is the function you should run when the die is clicked, which is the hold function
+        <button className="die-button" style={styles} onClick={() => props.holdDice(props.id)}>
             {props.value}
         </button>
     )
