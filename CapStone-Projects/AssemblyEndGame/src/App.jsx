3746dd1be4e83965fb4d8bccbcdeff2a1fce724e
cmvinayak <cmvinayak04@gmail.com>
2026-02-24 17:53:27 +0530

    Added 'Word' for the game by mapping it out from the react state.

--- CapStone-Projects/AssemblyEndGame/src/App.jsx
+++ CapStone-Projects/AssemblyEndGame/src/App.jsx
@@ -4,37 +4,47 @@ import { languages } from '../languages'
 
 export default function App() {
 
-  const state = true
-  const styles = {backgroundColor: state ? 'green' : 'red'}
+    const [currentWord, setCurrentWord] = useState("vampire")
+    
+    const state = true
+    const styles = {backgroundColor: state ? 'green' : 'red'}
 
-  return (
-    <>
-    <main>
-        <header>
-            <h1>Assembly: End Game</h1>
-            <p>Guess the word within 8 attempts to keep the 
-                programming world safe from Assembly!</p>
-        </header>
+    return (
+      <>
+      <main>
+          <header>
+              <h1>Assembly: End Game</h1>
+              <p>Guess the word within 8 attempts to keep the 
+                  programming world safe from Assembly!</p>
+          </header>
 
-        <section className="status" style={styles}>
-            <h3>{state ? "GAME WON!" : "Game over!"}</h3>
-        </section>
+          <section className="status" style={styles}>
+              <h3>{state ? "GAME WON!" : "Game over!"}</h3>
+          </section>
 
-        <section className='languages'>
-            {languages.map(lang => (
-                <div className='lang' 
-                     style={{backgroundColor: lang.backgroundColor, color: lang.color}}
-                     key={lang.name}
+          <section className='languages'>
+              {languages.map(lang => (
+                  <div className='lang' 
+                       style={{backgroundColor: lang.backgroundColor, color: lang.color}}
+                       key={lang.name}
                                       >{lang.name}
-                </div>
-            ))}
-        </section>
+                  </div>
+              ))}
+          </section>
+
+          <section className="word">
+                {currentWord.split("").map((letter, index) => (
+                        <span className="letter" key={index}>
+                                {letter.toUpperCase()}
+                        </span>
+                    ))}
+         </section>
 
 
 
-    </main>
-    </>
-  )
+      </main>
+      </>
+    )
 }
 
 
